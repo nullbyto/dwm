cc6a7e434741b392aa0316eed4c49d4fca5669cc
nullbyto <nullbyto@gmail.com>
2023-08-26 13:44:23 +0200

    patch: movestack

--- config.h
+++ config.h
@@ -112,8 +112,8 @@ static const char *playpause[] = { "playerctl", "play-pause", NULL };
 static const char *nexttrack[] = { "playerctl", "next", NULL };
 static const char *prevtrack[] = { "playerctl", "previous", NULL };
 
-/* multimedia keys */
-#include <X11/XF86keysym.h>
+#include <X11/XF86keysym.h> /* multimedia keys */
+#include "movestack.c"
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -129,6 +129,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
